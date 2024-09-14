1c73e6b5f86e311d758f1d4a31d5522f7e3798f3
~ ismaeL_RAM <133639910+RamadanismaeL@users.noreply.github.com>
2024-09-14 22:01:33 +0200

    Completed...

---
+++ Version 1.21/Proj_BlueMoon/src/Abstrato/Config_idiomas.java
@@ -0,0 +1,9 @@
+package Abstrato;
+
+/**
+ * @author Ramadan ismaeL
+ */
+public interface Config_idiomas {
+    public abstract void configPort();
+    public abstract void configEng();
+}
---
+++ Version 1.21/Proj_BlueMoon/src/Abstrato/Config_remover.java
@@ -0,0 +1,17 @@
+package Abstrato;
+
+/**
+ * @author Ramadan ismaeL
+ */
+public interface Config_remover {
+    public abstract void remover1();
+    public abstract void remover2();
+    public abstract void remover3();
+    public abstract void remover4();
+    public abstract void remover5();
+    public abstract void remover6();
+    public abstract void remover7();
+    public abstract void remover8();
+    public abstract void remover9();
+    public abstract void remover10();
+}
